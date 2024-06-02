9a77b81308199916a98e8aab6592678b1fc99efa
genesisxyz <colludamiano7@gmail.com>
2024-06-02 18:56:15 +0200

    add coins list with market data query

---
+++ src/api/coins/getCoinsMarkets.ts
@@ -0,0 +1,57 @@
+import apiClient from '../../axios';
+import { CoinMarket } from '../../types/CoinMarket';
+
+export type GetCoinsMarketsPayload = {
+  /**
+   * target currency of coins and market data.
+   * refers to https://docs.coingecko.com/reference/simple-supported-currencies.
+   */
+  vs_currency: string;
+  /**
+   * coins' ids, comma-separated if querying more than 1 coin.
+   * refers to https://docs.coingecko.com/reference/coins-list.
+   */
+  ids?: string;
+  /**
+   * filter based on coins' category.
+   * refers to https://docs.coingecko.com/reference/coins-categories-list.
+   */
+  category?: string;
+  /**
+   * sort result by field, default: market_cap_desc
+   */
+  order?: string;
+  /**
+   * total results per page, default: 100
+   * Valid values: 1...250
+   */
+  per_page?: number;
+  /**
+   * page through results, default: 1
+   */
+  page?: number;
+  /**
+   * include sparkline 7 days data, default: false
+   */
+  sparkline?: boolean;
+  /**
+   * include price change percentage timeframe, comma-separated if query more than 1 price change percentage timeframe
+   * Valid values: 1h, 24h, 7d, 14d, 30d, 200d, 1y
+   */
+  price_change_percentage?: string;
+  /**
+   * language background, default: en
+   */
+  locale?: string;
+  /**
+   * decimal place for currency price value
+   */
+  precision?: string;
+};
+
+export const getCoinsMarkets = async (payload: GetCoinsMarketsPayload) => {
+  const response = await apiClient.get<CoinMarket[]>('/coins/markets', {
+    params: payload,
+  });
+  return response.data;
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,18 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useQuery } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { getCoinsMarkets } from '../api/coins/getCoinsMarkets';
 import { RootStackParamList } from '../routes';
 
 export type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export default function HomeScreen(props: HomeScreenProps) {
+  const { data } = useCoinsListWithMarketDataQuery();
+
+  console.log(data?.map((e) => e.name));
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,6 +20,22 @@ export default function HomeScreen(props: HomeScreenProps) {
     </View>
   );
 }
+
+export const useCoinsListWithMarketDataQuery = () => {
+  return useQuery({
+    queryKey: ['coins', 'markets'],
+    queryFn: async () => {
+      const response = await getCoinsMarkets({
+        vs_currency: 'usd',
+        order: 'market_cap_desc',
+        per_page: 10,
+        page: 1,
+        sparkline: false,
+      });
+      return response;
+    },
+  });
+};
 
 const styles = StyleSheet.create({
   container: {
