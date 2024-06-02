fc819ecc43761502e019d9eafc0e068ce6ee447c
genesisxyz <colludamiano7@gmail.com>
2024-06-02 17:34:34 +0200

    add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier', 'simple-import-sort'],
+  rules: {
+    'prettier/prettier': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+  },
+};
