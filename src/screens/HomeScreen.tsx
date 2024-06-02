65c4ba37d97f5080e3160f07b05fff3490dc9638
genesisxyz <colludamiano7@gmail.com>
2024-06-02 19:04:34 +0200

    display top 10 coins on a list

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,35 @@
+import { Box } from '@gluestack-ui/themed';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useQuery } from '@tanstack/react-query';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { FlatList, FlatListProps, Text } from 'react-native';
 
 import { getCoinsMarkets } from '../api/coins/getCoinsMarkets';
 import { RootStackParamList } from '../routes';
+import { CoinMarket } from '../types/CoinMarket';
 
 export type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export default function HomeScreen(props: HomeScreenProps) {
   const { data } = useCoinsListWithMarketDataQuery();
 
-  console.log(data?.map((e) => e.name));
+  const renderItem = useCallback<NonNullable<FlatListProps<CoinMarket>['renderItem']>>((info) => {
+    const { item } = info;
+
+    return (
+      <Box flexDirection="row" padding={16} borderBottomWidth={1} borderBottomColor="gray.200">
+        <Text>{item.name}</Text>
+      </Box>
+    );
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      contentInsetAdjustmentBehavior="automatic"
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
   );
 }
 
@@ -36,12 +48,3 @@ export const useCoinsListWithMarketDataQuery = () => {
     },
   });
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
