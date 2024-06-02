5baaa4fd7353955409ef6a81f4f04ebd2b454517
genesisxyz <colludamiano7@gmail.com>
2024-06-02 18:49:20 +0200

    add gluestack for UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
 import { i18n } from '@lingui/core';
 import { I18nProvider, TransRenderProps } from '@lingui/react';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Text } from 'react-native';
 
 import HomeStack from './navigators/HomeStack';
 
@@ -16,9 +17,11 @@ export default function App() {
   return (
     <I18nProvider i18n={i18n} defaultComponent={DefaultComponent}>
       <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <HomeStack />
-        </NavigationContainer>
+        <GluestackUIProvider config={config}>
+          <NavigationContainer>
+            <HomeStack />
+          </NavigationContainer>
+        </GluestackUIProvider>
       </QueryClientProvider>
     </I18nProvider>
   );
