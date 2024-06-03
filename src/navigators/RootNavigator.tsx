a97225448f4775fe54ecd7b0e90531ac3fb82769
genesisxyz <colludamiano7@gmail.com>
2024-06-03 19:43:33 +0200

    allow tap on crypto list item

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -3,6 +3,7 @@ import { useLingui } from '@lingui/react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '../routes';
+import DetailScreen from '../screens/DetailScreen';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -13,6 +14,7 @@ export default function RootNavigator() {
   return (
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} options={{ title: _(msg`Home`) }} />
+      <Stack.Screen name="Detail" component={DetailScreen} options={{ title: _(msg`Detail`) }} />
     </Stack.Navigator>
   );
 }
---
+++ src/screens/DetailScreen.tsx
@@ -0,0 +1,9 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { RootStackParamList } from '../routes';
+
+export type DetailScreenProps = NativeStackScreenProps<RootStackParamList, 'Detail'>;
+
+export default function DetailScreen(props: DetailScreenProps) {
+  return null;
+}
