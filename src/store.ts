6520861766c442674599481aa3f151bfe3b2b4ba
genesisxyz <colludamiano7@gmail.com>
2024-06-05 17:03:45 +0200

    add AsyncStorage for persistent storage

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,8 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+import { createEnhancedJSONStorage } from './utils/createEnhancedJSONStorage';
 
 type Store = {
   favoriteCoins: Map<string, { id: string; name: string }>;
@@ -6,20 +10,28 @@ type Store = {
   removeCoin: (coinId: string) => void;
 };
 
-export const useStore = create<Store>((set) => ({
-  favoriteCoins: new Map(),
-  addCoin: (coin) =>
-    set((state) => {
-      return {
-        favoriteCoins: new Map(state.favoriteCoins).set(coin.id, coin),
-      };
+export const useStore = create<Store, [['zustand/persist', Store]]>(
+  persist(
+    (set) => ({
+      favoriteCoins: new Map(),
+      addCoin: (coin) =>
+        set((state) => {
+          return {
+            favoriteCoins: new Map(state.favoriteCoins).set(coin.id, coin),
+          };
+        }),
+      removeCoin: (coinId: string) =>
+        set((state) => {
+          const favoriteCoins = new Map(state.favoriteCoins);
+          favoriteCoins.delete(coinId);
+          return {
+            favoriteCoins,
+          };
+        }),
     }),
-  removeCoin: (coinId: string) =>
-    set((state) => {
-      const favoriteCoins = new Map(state.favoriteCoins);
-      favoriteCoins.delete(coinId);
-      return {
-        favoriteCoins,
-      };
-    }),
-}));
+    {
+      name: 'storage',
+      storage: createEnhancedJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
---
+++ src/utils/createEnhancedJSONStorage.ts
@@ -0,0 +1,57 @@
+import { PersistStorage, StateStorage, StorageValue } from 'zustand/middleware';
+
+// https://github.com/pmndrs/zustand/pull/1763#issuecomment-1534779266
+
+function replacer(key: string, value: any) {
+  if (value instanceof Map) {
+    return { __type: 'Map', value: Object.fromEntries(value) };
+  }
+  if (value instanceof Set) {
+    return { __type: 'Set', value: Array.from(value) };
+  }
+  return value;
+}
+
+function reviver(key: string, value: any) {
+  if (value?.__type === 'Map') {
+    return new Map(Object.entries(value.value));
+  }
+  if (value?.__type === 'Set') {
+    return new Set(value.value);
+  }
+  return value;
+}
+
+export function createEnhancedJSONStorage<S>(
+  getStorage: () => StateStorage,
+): PersistStorage<S> | undefined {
+  let storage: StateStorage | undefined;
+
+  try {
+    storage = getStorage();
+  } catch {
+    // prevent error if the storage is not defined (e.g. when server side rendering a page)
+    return;
+  }
+
+  const persistStorage: PersistStorage<S> = {
+    getItem: (name) => {
+      const parse = (str: string | null) => {
+        if (str === null) {
+          return null;
+        }
+        return JSON.parse(str, reviver) as StorageValue<S>;
+      };
+      const str = (storage as StateStorage).getItem(name) ?? null;
+      if (str instanceof Promise) {
+        return str.then(parse);
+      }
+      return parse(str);
+    },
+    setItem: (name, newValue) =>
+      (storage as StateStorage).setItem(name, JSON.stringify(newValue, replacer)),
+    removeItem: (name) => (storage as StateStorage).removeItem(name),
+  };
+
+  return persistStorage;
+}
