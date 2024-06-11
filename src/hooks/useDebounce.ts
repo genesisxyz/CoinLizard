b39851c327f4a91778ffb927c6207d097ca67994
genesisxyz <colludamiano7@gmail.com>
2024-06-11 22:24:27 +0200

    add tests for useDebounce hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export function useDebounce(value: any, delay: number) {
+export function useDebounce<T>(value: T, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
