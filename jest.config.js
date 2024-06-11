177951b60be04b9790f93f28d3d5d6c91ecd77d0
genesisxyz <colludamiano7@gmail.com>
2024-06-11 21:53:19 +0200

    update Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,16 @@ module.exports = {
   ...expoPreset,
   preset: '@testing-library/react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  setupFilesAfterEnv: ['./jest/setup.ts'],
+  setupFilesAfterEnv: ['./jest-setup.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg||@legendapp/*|expo-linear-gradient)',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.ts',
   ],
 };
