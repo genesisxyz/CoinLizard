ce2fa58634fc084b82e33cf369d8f39188174bc2
genesisxyz <colludamiano7@gmail.com>
2024-06-13 09:30:15 +0200

    update app.json to typescript version

---
+++ app.config.ts
@@ -0,0 +1,46 @@
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+
+import pkg from './package.json';
+
+const VERSION = pkg.version;
+
+export default (_: ConfigContext): ExpoConfig => ({
+  name: 'coinlizard',
+  slug: 'coinlizard',
+  version: VERSION,
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.genesisxyz.coinlizard',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    package: 'com.genesisxyz.coinlizard',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['@config-plugins/detox'],
+  extra: {
+    eas: {
+      projectId: 'da41b593-6838-44cf-a4e3-1c5d5f886903',
+    },
+  },
+  updates: {
+    url: 'https://u.expo.dev/da41b593-6838-44cf-a4e3-1c5d5f886903',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
+  owner: 'genesisxyz',
+});
