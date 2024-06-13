cbe6e3fd85d87924c610d46287362ae4234acd8e
genesisxyz <colludamiano7@gmail.com>
2024-06-13 09:30:45 +0200

    remove console logs on production build

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.transformer.minifierConfig = {
+  compress: {
+    // The option below removes all console logs statements in production.
+    drop_console: true,
+  },
+};
+
 module.exports = config;
